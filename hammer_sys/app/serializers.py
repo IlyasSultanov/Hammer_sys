88975698b76cb8718ad43e70d2b39ba70fff76a9
Ilyas <SultanovIlyas0.com>
2025-07-24 13:14:26 +0500

    fead: Serializers data

--- hammer_sys/app/serializers.py
+++ hammer_sys/app/serializers.py
@@ -4,6 +4,8 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from .models import User
 
+"""Сериализация данных из модели"""
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,10 +17,14 @@ class UserSerializer(serializers.ModelSerializer):
             'number': {'required': True}
         }
 
+    """Валидация данных для корректного ввода number"""
+
     def validate(self, data):
         if not data.get('email') and not data.get('number'):
             raise ValidationError("Укажите email или номер телефона")
         return data
+
+    """Валидация данных для корректного ввода email"""
 
     def validate_email(self, value):
         if value:
@@ -27,6 +33,8 @@ class UserSerializer(serializers.ModelSerializer):
             except ValidationError:
                 raise serializers.ValidationError("Некорректный email адрес")
         return value
+
+    """Создание пользователя"""
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
